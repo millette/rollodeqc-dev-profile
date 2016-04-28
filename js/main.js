0e5d461040c4289d67657bc28d4bf43ed19be635
Robin Millette <robin@millette.info>
2016-04-28 06:02:27 -0400

    More fake data; code reformatting.

--- js/main.js
+++ js/main.js
@@ -1,50 +1,43 @@
 /* globals data1, data2, d3, $ */
 'use strict'
 
+// Adapted from http://bl.ocks.org/mbostock/3884955
 const grapher = (data, sel) => {
-  const margin = { top: 20, right: 80, bottom: 30, left: 50 }
+  const margin = { top: 20, right: 30, bottom: 30, left: 50 }
   const width = 460 - margin.left - margin.right
   const height = 250 - margin.top - margin.bottom
-
-  const x = d3.scale.linear()
-      .range([0, width])
-
-  const y = d3.scale.linear()
-      .range([height, 0])
-
+  const x = d3.scale.linear().range([0, width])
+  const y = d3.scale.linear().range([height, 0])
   const color = d3.scale.category10()
 
   const xAxis = d3.svg.axis()
-      .scale(x)
-      .orient('bottom')
+    .scale(x)
+    .orient('bottom')
 
   const yAxis = d3.svg.axis()
-      .scale(y)
-      .orient('left')
+    .scale(y)
+    .orient('left')
 
   const line = d3.svg.line()
-      .interpolate('basis')
-      .x((d) => x(d.date))
-      .y((d) => y(d.temperature))
+    .interpolate('basis')
+    .x((d) => x(d.date))
+    .y((d) => y(d.temperature))
 
   const svg = d3.select(sel).append('svg')
-      // .attr('width', width + margin.left + margin.right)
-      // .attr('height', height + margin.top + margin.bottom)
-      .attr('width', '100%')
-      .attr('height', '100%')
-      .attr('viewBox', '0 0 ' + (width + margin.left + margin.right) + ' ' + (height + margin.top + margin.bottom))
-      .attr('preserveAspectRatio', 'xMidYMid meet')
-    .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    .attr('width', '100%')
+    .attr('height', '100%')
+    .attr('viewBox', [
+      '0 0', width + margin.left + margin.right, height + margin.top + margin.bottom
+    ].join(' '))
+    .attr('preserveAspectRatio', 'xMidYMid meet')
+    .append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
   color.domain(d3.keys(data[0]).filter((key) => key !== 'date'))
 
   const cities = color.domain().map((name) => {
     return {
       name: name,
-      values: data.map((d) => {
-        return {date: d.date, temperature: +d[name]}
-      })
+      values: data.map((d) => { return {date: d.date, temperature: +d[name]} })
     }
   })
 
@@ -56,40 +49,27 @@ const grapher = (data, sel) => {
   ])
 
   svg.append('g')
-      .attr('class', 'x axis')
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(xAxis)
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis)
 
   svg.append('g')
-      .attr('class', 'y axis')
-      .call(yAxis)
-    .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '.71em')
-      .style('text-anchor', 'end')
-      .text('Number')
+    .attr('class', 'y axis')
+    .call(yAxis)
 
-  const city = svg.selectAll('.city')
-      .data(cities)
-    .enter().append('g')
-      .attr('class', 'city')
+  const city = svg.selectAll('.city').data(cities)
+    .enter().append('g').attr('class', 'city')
 
   city.append('path')
-      .attr('class', 'line')
-      .attr('d', (d) => line(d.values))
-      .style('stroke', (d) => color(d.name))
+    .attr('class', 'line')
+    .attr('d', (d) => line(d.values))
+    .style('stroke', (d) => color(d.name))
 
   city.append('text')
-      .datum((d) => {
-        return {name: d.name, value: d.values[d.values.length - 1]}
-      })
-      .attr('transform',
-        (d) => 'translate(' + x(d.value.date) + ',' + y(d.value.temperature) + ')'
-      )
-      .attr('x', 3)
-      .attr('dy', '.35em')
-      .text((d) => d.name)
+    .datum((d) => { return {name: d.name, value: d.values[d.values.length - 1]} })
+    .attr('transform',
+      (d) => 'translate(' + x(d.value.date) + ',' + y(d.value.temperature) + ')'
+    )
 }
 
 $(() => {
