7f90d0f183556c2b3c4565ca67cb9e897bd2117e
Robin Millette <robin@millette.info>
2016-05-10 11:16:48 -0400

    With event type graph.

--- js/main.js
+++ js/main.js
@@ -1,72 +1,6 @@
 'use strict'
 
 const utils = require('./utils.js')
-const vg = require('vega')
-vg.embed = require('vega-embed')
-vg.lite = require('vega-lite')
-
-const cleanHTML = (h) => h
-  .replace(/&/g, '&amp;')
-  .replace(/"/g, '&quot;')
-  .replace(/'/g, '&#39;')
-  .replace(/</g, '&lt;')
-  .replace(/>/g, '&gt;')
-
-const countActions = (actions) => {
-  if (actions === undefined || actions === true) { return 3 }
-  if (actions === false || typeof actions !== 'object') { return 0 }
-
-  var r
-  var trues = 3
-  for (r in actions) {
-    if (!actions[r]) { --trues }
-  }
-  console.log('trues:', trues)
-  return trues
-}
-
-const addVega = (el, spec) => {
-  const s = {
-    mode: 'vega-lite',
-    spec: spec,
-    // actions: { editor: false, source: false, export: false },
-    renderer: 'svg'
-  }
-  const vegaEl = document.createElement('div')
-  vg.embed(vegaEl, s, (err, result) => {
-    const elEl = document.querySelector(el)
-    if (err) {
-      const preEl = document.createElement('pre')
-      preEl.innerHTML = err
-      elEl.appendChild(preEl)
-      return
-    }
-    const titleEl = document.createElement('h5')
-    titleEl.style.marginBottom = 0
-    titleEl.style.textAlign = 'center'
-    titleEl.innerHTML = cleanHTML(spec.title || spec.description)
-    elEl.appendChild(titleEl)
-    elEl.appendChild(vegaEl)
-    const svg = result.view._el.firstChild
-    const width = svg.getAttribute('width')
-    const height = svg.getAttribute('height')
-    svg.setAttribute('viewBox', `0 0 ${width} ${height}`)
-    svg.setAttribute('width', '100%')
-    svg.setAttribute('height', '100%')
-    const actionsEl = document.querySelectorAll('.vega-actions')
-    if (actionsEl) {
-      const nColsClasses = [ false, 'one', 'halves', 'thirds' ]
-      const colClass = nColsClasses[countActions(s.actions)]
-      if (colClass) {
-        var r
-        for (r = 0; r < actionsEl.length; ++r) {
-          // console.log('actionsEl:', typeof actionsEl, actionsEl.length, actionsEl)
-          actionsEl[r].classList.add(colClass)
-        }
-      }
-    }
-  })
-}
 
 const datas = [
   window.fetch('./events.json').then((response) => response.json()),
@@ -75,10 +9,8 @@ const datas = [
 
 document.addEventListener('DOMContentLoaded', () => {
   Promise.all(datas).then((data) => {
-    const json = data[0]
-    const repos = data[1]
-    addVega('#vega-lite', utils.hourPeriod('Last events, hours worked', json))
-    addVega('#vega-lite2', utils.dayPeriod('Last events, days worked', json))
+    utils.addVega('#vega-lite', utils.eventTypeBars('By event type', data[0]))
+    utils.addVega('#vega-lite2', utils.licenseBars('Licenses, by number of projects', data[1]))
   })
   .catch((ex) => { console.log('parsing failed', ex) })
 })
--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,68 @@
 'use strict'
 
+const vg = require('vega')
+vg.embed = require('vega-embed')
+
+const cleanHTML = (h) => h
+  .replace(/&/g, '&amp;')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&#39;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+
+const countActions = (actions) => {
+  if (actions === undefined || actions === true) { return 3 }
+  if (actions === false || typeof actions !== 'object') { return 0 }
+
+  var r
+  var trues = 3
+  for (r in actions) { if (!actions[r]) { --trues } }
+  return Math.max(trues, 0)
+}
+
+exports.addVega = (el, spec) => {
+  const s = {
+    mode: 'vega-lite',
+    spec: spec,
+    actions: { source: false },
+    renderer: 'svg'
+  }
+
+  const vegaEl = document.createElement('div')
+  vg.embed(vegaEl, s, (err, result) => {
+    const elEl = document.querySelector(el)
+    if (err) {
+      const preEl = document.createElement('pre')
+      preEl.innerHTML = err
+      elEl.appendChild(preEl)
+      return
+    }
+    const titleEl = document.createElement('h5')
+    titleEl.style.marginBottom = 0
+    titleEl.style.textAlign = 'center'
+    titleEl.innerHTML = cleanHTML(spec.title || spec.description)
+    elEl.appendChild(titleEl)
+    elEl.appendChild(vegaEl)
+    const svg = result.view._el.firstChild
+    const width = svg.getAttribute('width')
+    const height = svg.getAttribute('height')
+    svg.setAttribute('viewBox', `0 0 ${width} ${height}`)
+    svg.setAttribute('width', '100%')
+    svg.setAttribute('height', '100%')
+    const actionsEl = document.querySelectorAll('.vega-actions')
+    if (actionsEl) {
+      const nColsClasses = [ false, 'one', 'halves', 'thirds' ]
+      const colClass = nColsClasses[countActions(s.actions)]
+      if (colClass) {
+        var r
+        for (r = 0; r < actionsEl.length; ++r) { actionsEl[r].classList.add(colClass) }
+      }
+    }
+  })
+}
+
 // period: day or hours
-exports.byPeriod = (period) => {
+const byPeriod = (period) => {
   return {
     mark: 'bar',
     encoding: {
@@ -13,15 +74,47 @@ exports.byPeriod = (period) => {
 }
 
 exports.dayPeriod = (description, json) => {
-  const out = exports.byPeriod('day')
+  const out = byPeriod('day')
   out.description = description
   out.data = { values: json }
   return out
 }
 
 exports.hourPeriod = (description, json) => {
-  const out = exports.byPeriod('hours')
+  const out = byPeriod('hours')
   out.description = description
   out.data = { values: json }
   return out
+}
+
+exports.licenseBars = (description, json) => {
+  return {
+    description: description,
+    data: { values: json },
+    mark: 'bar',
+    transform: {
+      calculate: [
+        { field: 'Licenses', expr: '(datum.license && datum.license.key) || "unknown"' },
+        { field: 'Source', expr: 'datum.fork ? "Fork" : "Original"' }
+      ]
+    },
+    encoding: {
+      y: { field: 'Licenses', type: 'ordinal' },
+      x: { aggregate: 'count', field: '*', type: 'quantitative' },
+      color: { field: 'Source', type: 'nominal' }
+    }
+  }
+}
+
+exports.eventTypeBars = (description, json) => {
+  return {
+    description: description,
+    data: { values: json },
+    mark: 'bar',
+    encoding: {
+      y: { field: 'type', type: 'ordinal' },
+      x: { aggregate: 'count', field: '*', type: 'quantitative' },
+      color: { field: 'type', type: 'nominal' }
+    }
+  }
 }
