22805c5c7767788d4fc385a76b4afdf77900217c
Robin Millette <robin@millette.info>
2016-05-11 15:02:07 -0400

    A little reformatting.

--- webpack.config.js
+++ webpack.config.js
@@ -25,23 +25,22 @@ const preprocess = (dev) => {
 
 const devs = preprocess(require('./user.json'))
 
-const loaders = [ { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' } ]
+const loaders = [
+  { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
+  { test: /\/js\/(main|utils)\.js$/, loader: 'babel?presets[]=es2015' }
+]
+
 const plugins = [ new webpack.ProvidePlugin({
   'Promise': 'imports?this=>global!exports?global.Promise!es6-promise',
   'window.fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
 }) ]
-
-loaders.push({ test: /\/js\/(main|utils)\.js$/, loader: 'babel?presets[]=es2015' })
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }))
 }
 
 module.exports = {
-  entry: [
-    './entry.js',
-    'file?name=index.html!jade-html!./index.jade'
-  ],
+  entry: [ './entry.js', 'file?name=index.html!jade-html!./index.jade' ],
   output: { path: __dirname, filename: 'bundle.js' },
   devServer: { inline: true, host: '0.0.0.0', port: 1234 },
   module: { loaders: loaders },
